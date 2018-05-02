89a8cabc3e2f6ad1347b2ad9dced3291422458dc
heze <www.895315401@qq.com>
2018-05-02 17:54:11 +0800

    Initial Commit

---
+++ rotation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rotation
+//
+//  Created by 何泽的mac on 2018/5/2.
+//  Copyright © 2018年 何泽的mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rotation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  rotation
+//
+//  Created by 何泽的mac on 2018/5/2.
+//  Copyright © 2018年 何泽的mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
