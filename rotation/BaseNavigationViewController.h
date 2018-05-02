33fb56892b946daf1829ce15314231a3d4f1d1c9
heze <www.895315401@qq.com>
2018-05-02 23:06:39 +0800

    提交

---
+++ rotation/BaseNavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseNavigationViewController.h
+//  rotation
+//
+//  Created by 何泽的mac on 2018/5/2.
+//  Copyright © 2018年 何泽的mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseNavigationViewController : UINavigationController
+
+@end
---
+++ rotation/testViewController.h
@@ -0,0 +1,13 @@
+//
+//  testViewController.h
+//  rotation
+//
+//  Created by 何泽的mac on 2018/5/2.
+//  Copyright © 2018年 何泽的mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testViewController : UIViewController
+
+@end
